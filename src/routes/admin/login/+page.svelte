9c12d5ca6662fddbd99c5fab2ba663ec42e33094
imtiaj <imtiajrex@gmail.com>
2022-12-28 19:22:48 +0600

    added admin login page

---
+++ src/routes/admin/login/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+	// @ts-nocheck
+	import Button from '$lib/components/button.svelte';
+	import Input from '$lib/components/input.svelte';
+	let username = '';
+	let password = '';
+	const handleClick = (e) => {
+		e.preventDefault();
+		console.log(username, password);
+	};
+</script>
+
+<div class="wrapper">
+	<form class="container">
+		<h1>Admin Sign In</h1>
+		<Input label="Username" name="username" type="text" bind:value={username} />
+		<Input label="Password" name="password" type="password" bind:value={password} />
+		<Button text="Sign In" onClick={handleClick} />
+	</form>
+</div>
+
+<style>
+	.wrapper {
+		min-height: 100vh;
+		width: 100%;
+		background-color: #232932;
+		display: flex;
+		justify-content: center;
+	}
+	.container {
+		margin-top: 25%;
+		max-width: 450px;
+		width: 100%;
+	}
+	h1 {
+		color: white;
+		font-weight: bold;
+		text-align: center;
+		margin-bottom: 3rem;
+		font-size: 45px;
+	}
+</style>

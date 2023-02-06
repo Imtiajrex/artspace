35c8e804d50410e65c60e7792dad869ce32adfe3
Imtiaj <imtiajrex@gmail.com>
2023-02-06 13:56:30 +0600

    updated dashboard page

---
+++ src/lib/components/admin/dashboard/StatCard.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { Icon123, IconUser } from '@tabler/icons-svelte';
+	export let Icon = IconUser;
+	export let name = 'Users';
+	export let numbers = '100+';
+</script>
+
+<div class="card">
+	<div class="icon">
+		<Icon />
+	</div>
+	<div>
+		<h3>{numbers}</h3>
+		<p>{name}</p>
+	</div>
+</div>
+
+<style>
+	.card {
+		display: flex;
+		padding: 20px;
+		width: max-content;
+		border-radius: 14px;
+		background: var(--bg);
+		min-width: 220px;
+		align-items: center;
+	}
+	.icon {
+		margin-right: 15px;
+		width: 40px;
+		height: 40px;
+		border-radius: 50%;
+		background-color: var(--primary);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	h3 {
+		font-size: 25px;
+		font-weight: bold;
+	}
+	p {
+		font-size: 15px;
+		color: #a1a1a1;
+	}
+</style>
---
+++ src/routes/admin/dashboard/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+	import StatCard from '$lib/components/admin/dashboard/StatCard.svelte';
+	import Button from '$lib/components/button.svelte';
+	import Input from '$lib/components/input.svelte';
+	import Logo from '$lib/components/logo.svelte';
+	import { IconMoodAngry, IconPaint, IconUser } from '@tabler/icons-svelte';
+	let name = 'Imtiajrex';
+</script>
+
+<div class="navbar">
+	<Logo />
+	<IconUser />
+</div>
+<div class="container">
+	<h1>Welcome {name}</h1>
+	<p>Admin Dashboard</p>
+</div>
+<div class="card-container">
+	<StatCard name="Users" />
+	<StatCard name="Arts" numbers="500+" Icon={IconPaint} />
+	<StatCard name="Moderation Needed" numbers="500+" Icon={IconMoodAngry} />
+</div>
+
+<style>
+	.navbar {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem;
+	}
+	.container {
+		margin-top: 5vh;
+	}
+	h1 {
+		margin-bottom: 20px;
+		font-size: 40px;
+		font-weight: regular;
+	}
+	p {
+		color: #a1a1a1;
+		font-size: 20px;
+	}
+	.card-container {
+		padding: 20px;
+		margin-top: 40px;
+		display: flex;
+		gap: 20px;
+	}
+</style>

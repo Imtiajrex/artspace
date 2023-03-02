82f70b1fce04c66fb137f56677cbd7aa3d34a4f4
mdriduanislam <mohhammadris786@gmail.com>
2023-03-02 12:49:00 +0600

    added user sign in

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,79 @@
+<script>
+	// @ts-nocheck
+	import Button from '$lib/components/button.svelte';
+	import Input from '$lib/components/input.svelte';
+	let username = '';
+	let password = '';
+	let email = '';
+	const handleClick = async (e) => {
+		e.preventDefault();
+		const res = await fetch('/api/signup', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				username,
+				password,
+				email
+			})
+		});
+		if (res.status != 200) {
+			const body = await res.json();
+			alert(body.message);
+			return;
+		}
+		window.location.href = '/signup';
+	};
+</script>
+
+<div class="wrapper">
+	<form class="container" on:submit={handleClick}>
+		<h1>Join Artspace to improve yourself and other artists</h1>
+		<Input label="Username" name="username" type="text" bind:value={username} />
+		<Input label="Email" name="email" type="email" bind:value={email} />
+		<Input label="Password" name="password" type="password" bind:value={password} />
+		<Button text="Sign Up" maxWidth="250px" />
+		<br>
+		<p>Already have an account?<a href="login">Sign In</a></p>
+	</form>
+	<img src="BG image.png" alt="">
+</div>
+
+<style>
+	.wrapper {
+		min-height: 100vh;
+		width: 100%;
+		background-color: var(--dark-bg);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.container {
+		max-width: 450px;
+		width: 60%;
+		justify-content: left;
+		margin-top:0;
+		margin-right: 20px;
+		
+	}
+	.img{
+        width: 40%;
+        justify-content: left;
+		
+
+	}
+	h1 {
+		color: white;
+		font-weight: bold;
+		text-align: center;
+		margin-bottom: 3rem;
+		font-size: 45px;
+	}
+	.a{
+		color:var(--primary);
+	}
+	button {
+		margin-inline: auto;
+	}
+</style>
